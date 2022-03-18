225abe3951d61fdb4d9335840e3d499c541599f1
rivmos <rivmos.official@gmail.com>
2022-03-18 16:14:13 +0530

    classes used in js and improved validation of forms plus table now scrollable

--- js/main.js
+++ js/main.js
@@ -9,61 +9,74 @@ class theLib{
     }
 }
 
-// Display construction function
-function Display(){
 
-}
-
-// function to add books to localStorage
-Display.prototype.addToLocal = function(eachBook){
-
-    if(localStorage.allBooksData){
-        allBooksArray = JSON.parse(localStorage.allBooksData);
-        allBooksArray.push(eachBook);
-        localStorage.setItem('allBooksData',JSON.stringify(allBooksArray))
+// Display Class
+class Display{
+    // function to add books to localStorage
+    addToLocal(eachBook){
+        if(localStorage.allBooksData){
+            let allBooksArray;
+            allBooksArray = JSON.parse(localStorage.allBooksData);
+            allBooksArray.push(eachBook);
+            localStorage.setItem('allBooksData',JSON.stringify(allBooksArray))
+        }
+        else{
+            let allBooksArray = [];     
+            allBooksArray.push(eachBook);
+            localStorage.setItem('allBooksData',JSON.stringify(allBooksArray))
+        }
     }
-    else{
-        let allBooksArray = [];     
-        allBooksArray.push(eachBook);
-        localStorage.setItem('allBooksData',JSON.stringify(allBooksArray))
+
+    // validating the entries in the field -  required more that 3 characters
+    validate(eachBook){
+        if(eachBook.bookName.length>3 && eachBook.authorName.length>3 && eachBook.radioAnswer!==undefined){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    // if the validate function returns false this gets executed with error message
+    error = function(){
+        let elem = document.getElementById('addSuccess');
+        let field = document.getElementsByClassName('field');
+
+        Array.from(field).forEach(function(element){
+            element.style.border = '1px solid rgb(250, 126, 126)';
+        })
+
+        elem.innerText = `Enter A Name With More Than 3 Characters`;
+        elem.style.backgroundColor = 'rgb(250, 126, 126)';
+        elem.style.color = 'rgb(73, 0, 0)';
+        elem.style.visibility = 'visible';
+        elem.style.opacity = '1';
+        setTimeout(function(){
+            elem.style.visibility = 'hidden';
+            elem.style.backgroundColor = 'rgb(94, 187, 94)';
+            elem.style.color = 'rgb(0, 70, 0)';
+            Array.from(field).forEach(function(element){
+                element.style.border = '1px solid transparent';
+            })
+            
+        },1000)
+        console.log(elem);
+    }
+
+    // to display a custom success message for each book added
+    success = function(eachBook){
+        let elem = document.getElementById('addSuccess');
+        elem.innerText = `${eachBook.bookName} Added!`;
+        elem.style.visibility = 'visible';
+        elem.style.opacity = '1';
+        setTimeout(function(){
+            elem.style.visibility = 'hidden';
+        },1000)
+        console.log(elem);
     }
 }
 
-// validating the entries in the field -  required more that 3 characters
-Display.prototype.validate = function(eachBook){
-    if(eachBook.bookName.length>3 && eachBook.authorName.length>3 && eachBook.radioAnswer!==undefined){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
-// if the validate function returns false this gets executed with error message
-Display.prototype.error = function(){
-    let elem = document.getElementById('addSuccess');
-    elem.innerText = `Enter Name With More Than 3 Characters`;
-    elem.style.backgroundColor = '#b64141';
-    elem.style.visibility = 'visible';
-    elem.style.opacity = '1';
-    setTimeout(function(){
-        elem.style.visibility = 'hidden';
-        elem.style.backgroundColor = 'rgb(65, 182, 65)';
-    },1000)
-    console.log(elem);
-}
-
-// to display a custom success message for each book added
-Display.prototype.success = function(eachBook){
-    let elem = document.getElementById('addSuccess');
-    elem.innerText = `${eachBook.bookName} Added!`;
-    elem.style.visibility = 'visible';
-    elem.style.opacity = '1';
-    setTimeout(function(){
-        elem.style.visibility = 'hidden';
-    },1000)
-    console.log(elem);
-}
 
 // grabbing the add button for click event
 let addBtn = document.getElementById('addBtn');
@@ -74,21 +87,24 @@ function addBook(){
     let bookName = document.getElementById('bookName');
     let authorName = document.getElementById('authorName');
     let radioAnswer = document.querySelector("input[name='bookCatagory']:checked");
-    let eachBook = new theLib(bookName.value,authorName.value,radioAnswer.value);
-
     let display = new Display();
-    if(display.validate(eachBook)){
-        display.addToLocal(eachBook);
-        display.success(eachBook);
+
+    if(radioAnswer){
+        let eachBook = new theLib(bookName.value,authorName.value,radioAnswer.value);
+        if(display.validate(eachBook)){
+            display.addToLocal(eachBook);
+            display.success(eachBook);
+        }
+        else{
+            display.error()
+        }
+        radioAnswer.checked = false;
     }
     else{
-        display.error()
+        display.error();
     }
-
     bookName.value = ''; 
     authorName.value = '';
-    radioAnswer.checked = false;
-
     showBooks();
 }
 
@@ -99,13 +115,6 @@ function showBooks(){
     let emptyDisplay = document.getElementById('emptyDisplay');
     emptyDisplay.style.display = 'none';
     let html = '';
-    html += `<tr>
-                <th>Book</th>
-                <th>Author</th>
-                <th>Type</th>
-            </tr>
-        `;
-    
     if(localStorage.allBooksData){
         allBooksData = JSON.parse(localStorage.allBooksData);
         allBooksData.forEach(function(element){
@@ -122,4 +131,4 @@ function showBooks(){
     else{
         emptyDisplay.style.display = 'block';
     }
-}
+} 
